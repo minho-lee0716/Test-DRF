d754ae84c7449c3a5ae9ee8720cae940cba08eae
Min-Ho Lee <minho.lee0716@gmail.com>
2020-11-15 23:56:38 +0900

    Add: test view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response   import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .serializers import (
+    ProductSerializer,
+    TagSerializer,
+    ProductOptionSerializer
+)
+from .models import (
+    Product,
+    Tag,
+    ProductOption
+)
+
+@api_view(['GET'])
+def testAPI(request):
+    return Response({'message':'SUCCESS'})
